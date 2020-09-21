a66d2d7692677f7b8137a2ee548194daea902877
Aleksei <alexsupa@mail.ru>
2020-09-21 20:45:46 +0300

    Updates

---
+++ precache-manifest.b4a0767a9713de3e9942eecffdf78e4a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "140c1538d661063f1094982ec363d044",
+    "url": "/test_Auth/index.html"
+  },
+  {
+    "revision": "2677bd3d575574c053a3",
+    "url": "/test_Auth/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c97cae954cc332f33ce8",
+    "url": "/test_Auth/static/css/main.bb4b23ef.chunk.css"
+  },
+  {
+    "revision": "2677bd3d575574c053a3",
+    "url": "/test_Auth/static/js/2.d279ce02.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/test_Auth/static/js/2.d279ce02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c97cae954cc332f33ce8",
+    "url": "/test_Auth/static/js/main.354f9343.chunk.js"
+  },
+  {
+    "revision": "7f7643e39c2d0381dfd7",
+    "url": "/test_Auth/static/js/runtime-main.3606e520.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test_Auth/static/media/logo.5d5d9eef.svg"
+  }
+]);
